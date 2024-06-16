e02af5f8920c61cca9a2c71baa25aa7ebca36bcf
Alex-GHP <alex.morariu.dev@gmail.com>
2024-06-16 21:53:15 +0300

    Smallest Multiple

---
+++ smallest-multiple.py
@@ -0,0 +1,14 @@
+#  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder 
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+import math
+
+def smallest_multiple(n):
+  lcm_value = 1
+  for i in range(2, n + 1):
+    gcd_value = math.gcd(lcm_value, i) 
+    lcm_value = (lcm_value * i) // gcd_value 
+  return lcm_value
+
+print(smallest_multiple(10))
